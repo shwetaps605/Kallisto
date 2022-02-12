6ef16a981ad1374b1be107413a017c6c36a8fe2b
shwetaps605 <shwetaroy305@gmail.com>
2022-02-12 21:58:31 +0530

    Added close functionality on click outside modal

--- src/components/modal/modal.component.jsx
+++ src/components/modal/modal.component.jsx
@@ -6,17 +6,20 @@ const Modal = (props) => {
     if (!props.showModal) return null
 
     return (
-        <div className="modal">
-            <div className="modal__header">
-                Modal Title
+        <div className="modal" onClick={props.onClose}>
+            <div className="modal__content" onClick={e => e.stopPropagation()}>
+                <div className="modal__header">
+                    Modal Title
+                </div>
+                <div className="modal__body">
+                    Modal Body
+                </div>
+                <div className="modal__footer">
+                    Model CTA/s
+                    <button onClick={props.onClose}>Close</button>
+                </div>
             </div>
-            <div className="modal__body">
-                Modal Body
-            </div>
-            <div className="modal__footer">
-                Model CTA/s
-                <button onClick={props.onClose}>Close</button>
-            </div>
+
         </div>
     )
 }
