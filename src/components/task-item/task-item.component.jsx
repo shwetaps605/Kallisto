f6d2d45302da790d5d08258367741d7cb2cffa3c
shwetaps605 <shwetaroy305@gmail.com>
2022-02-14 23:00:12 +0530

    Added option to view subtask

--- src/components/task-item/task-item.component.jsx
+++ src/components/task-item/task-item.component.jsx
@@ -5,10 +5,11 @@ const TaskItem = ({ task }) => (
     <div className="task__item">
         <h3>{task.taskTitle}</h3>
         <p>{task.createdAt}</p>
+        {task.subtasks.length == 0 && <p>No subtasks</p>}
         {
-            task.subtasks.length == 0 ?
-                <p>No subtasks</p> :
-                <p>{task.subtasks.lenth} subtasks</p>
+            task.subtasks.length > 0 && task.subtasks.map(subtask => (
+                <p>{subtask.subtaskTitle}</p>
+            ))
         }
     </div>
 )
