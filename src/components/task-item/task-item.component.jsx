3d4bf6bf8b0ebf8340a32eec7cdff150c866aaae
shwetaps605 <shwetaroy305@gmail.com>
2022-02-15 22:59:54 +0530

    Added notebook styling

--- src/components/task-item/task-item.component.jsx
+++ src/components/task-item/task-item.component.jsx
@@ -4,13 +4,19 @@ import './task-item.styles.scss'
 const TaskItem = ({ task }) => (
     <div className="task__item">
         <h3>{task.taskTitle}</h3>
-        <p>{task.createdAt}</p>
+        <div className="title-line"></div>
+
         {task.subtasks.length == 0 && <p>No subtasks</p>}
         {
             task.subtasks.length > 0 && task.subtasks.map(subtask => (
+                <>
                 <p>{subtask.subtaskTitle}</p>
+                <div className="blue-line"></div>
+                </>
+
             ))
         }
+        <p>{task.createdAt}</p>
     </div>
 )
 
