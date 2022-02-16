aef2b2bf2070c2a899c243bdeddeaea2058fd621
shwetaps605 <shwetaroy305@gmail.com>
2022-02-16 23:38:59 +0530

    Added function template for handling subtask status change

--- src/components/task-item/task-item.component.jsx
+++ src/components/task-item/task-item.component.jsx
@@ -1,35 +1,44 @@
 import React from "react"
 import './task-item.styles.scss'
 
-const TaskItem = ({ task }) => (
-    <div className="task__item">
+const TaskItem = ({ task }) => {
 
-        <div className="task__item__header__box">
-            <p id='date'>{task.createdAt}</p>
-            <h3>{task.taskTitle}</h3>
-            <div className="red-line"></div>
-        </div>
+    //TODO: Add this function in TasksContext
+    const handleSubtaskCompleteAction = () => {
+
+    }
 
 
-        {task.subtasks.length == 0 &&
-            <div className="task__item__body__box">
-                <p>No subtasks</p>
+    return (
+        <div className="task__item">
+
+            <div className="task__item__header__box">
+                <p id='date'>{task.createdAt}</p>
+                <h3>{task.taskTitle}</h3>
+                <div className="red-line"></div>
             </div>
-        }
-        {
-            task.subtasks.length > 0 && task.subtasks.map(subtask => (
+
+
+            {task.subtasks.length == 0 &&
                 <div className="task__item__body__box">
-                    <p>{subtask.subtaskTitle}</p>
-                    <svg className="svg" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
-                        <path d="M24 4.685l-16.327 17.315-7.673-9.054.761-.648 6.95 8.203 15.561-16.501.728.685z" />
-                    </svg>
-                    {/* <div className="blue-line"></div> */}
+                    <p>No subtasks</p>
                 </div>
+            }
+            {
+                task.subtasks.length > 0 && task.subtasks.map(subtask => (
+                    <div className="task__item__body__box">
+                        <p>{subtask.subtaskTitle}</p>
+                        <svg className="svg" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+                            <path d="M24 4.685l-16.327 17.315-7.673-9.054.761-.648 6.95 8.203 15.561-16.501.728.685z" />
+                        </svg>
+                        {/* <div className="blue-line"></div> */}
+                    </div>
 
-            ))
-        }
+                ))
+            }
 
-    </div>
-)
+        </div>
+    )
+}
 
 export default TaskItem
