1ecb9442e0cebcbbbb7bf8db4f5b67cb756def5d
shwetaps605 <shwetaroy305@gmail.com>
2022-02-16 23:31:09 +0530

    Added styles to the task-card-item

--- src/components/task-item/task-item.component.jsx
+++ src/components/task-item/task-item.component.jsx
@@ -3,20 +3,28 @@ import './task-item.styles.scss'
 
 const TaskItem = ({ task }) => (
     <div className="task__item">
-        <h3>{task.taskTitle}</h3>
-        <div className="title-line"></div>
+
+        <div className="task__item__header__box">
+            <p id='date'>{task.createdAt}</p>
+            <h3>{task.taskTitle}</h3>
+            <div className="red-line"></div>
+        </div>
+
 
         {task.subtasks.length == 0 && <p>No subtasks</p>}
         {
             task.subtasks.length > 0 && task.subtasks.map(subtask => (
-                <>
-                <p>{subtask.subtaskTitle}</p>
-                <div className="blue-line"></div>
-                </>
+                <div className="task__item__body__box">
+                    <p>{subtask.subtaskTitle}</p>
+                    <svg className="svg" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+                        <path d="M24 4.685l-16.327 17.315-7.673-9.054.761-.648 6.95 8.203 15.561-16.501.728.685z" />
+                    </svg>
+                    {/* <div className="blue-line"></div> */}
+                </div>
 
             ))
         }
-        <p>{task.createdAt}</p>
+
     </div>
 )
 
