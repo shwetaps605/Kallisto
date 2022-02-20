77cba8663e313d00d67d49ea886f0559eccd582d
shwetaps605 <shwetaroy305@gmail.com>
2022-02-20 16:05:10 +0530

    Added styles for task buttons

--- src/components/task-item/task-item.component.jsx
+++ src/components/task-item/task-item.component.jsx
@@ -40,6 +40,7 @@ const TaskItem = ({ task }) => {
                                 <path d="M24 4.685l-16.327 17.315-7.673-9.054.761-.648 6.95 8.203 15.561-16.501.728.685z" />
                             </svg>
                         }
+                        
 
                         {/* <div className="blue-line"></div> */}
                     </div>
--- src/components/task-list/task-list-card.component.jsx
+++ src/components/task-list/task-list-card.component.jsx
@@ -16,13 +16,20 @@ const TaskListCard = (props) => {
         <div className='task__list'>
             <AddTaskCard></AddTaskCard>
             <div className="task__list__container">
-                
+
                 {
                     tasks.map(task => (
-                        <div key={task.taskId}>
+                        <div key={task.taskId} className='task'>
                             <TaskItem task={task}></TaskItem>
-                            <button onClick={() => deleteTask(task.taskId)}>Delete</button>
-                            <button onClick={()=> props.onAddSubTask(task.taskId)}>Add Subtask</button>
+                            <div className="task__buttons">
+                                <button onClick={() => deleteTask(task.taskId)}>Delete</button>
+                                <button onClick={() => props.onAddSubTask(task.taskId)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                        <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z" />
+                                    </svg>
+                                </button>
+                            </div>
+
                         </div>
                     ))
                 }
